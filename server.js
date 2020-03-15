ec4fc933cf371fa5005d172bd5fa869017a8bb4a
Andrew Weihe <andrewy03@gmail.com>
2020-03-14 22:19:25 -0500

    deployed to heroku

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
-
-app.use(logger("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
@@ -15,18 +13,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODDB_URI = process.env.MONGODDB_URI || "mongodb://localhost/mongoHeadlines";
-
-mongoose.connect(MONGODDB_URI);
-
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+    useNewUrlParser: true,
+    useFindAndModify: false
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
